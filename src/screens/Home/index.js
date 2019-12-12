caeac96b2176d920f7ee77afa20b77d081b61405
Younes Henni <henni1989younes@gmail.com>
2019-12-12 20:10:46 +0000

    Finish line

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#a9a9a9',
-  },
-});
+import { Text } from 'react-native';
+import { Container } from './styledComponents';
 
 const Home = () => (
-  <View style={styles.container}>
-    <Text>Home</Text>
-  </View>
+  <Container>
+    <Text>Home Screen</Text>
+  </Container>
 );
 
 export default Home;
